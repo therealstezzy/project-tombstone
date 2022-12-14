38aa3ca548f4b69f60606899da00dc3d1a75723b
Chris <97630660+therealstezzy@users.noreply.github.com>
2022-12-13 19:45:51 -0800

    adding files

--- backend.py
+++ backend.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Person():
     def __init__(self, name, dob, dod, des) -> None:
         self.fullName = name
@@ -7,8 +9,10 @@ class Person():
     
     def getDescription(self):
         return self.description
+        
 
     
+data = [10,20,30,40,50,60]
+df = pd.DataFrame(data, columns=['Numbers'])
 
-
-
+print(df)
