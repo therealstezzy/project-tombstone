7630d346b2d41bc8d9ba5e1f402f040bcf036a94
Chris <97630660+therealstezzy@users.noreply.github.com>
2022-12-13 19:07:16 -0800

    changing main name

--- backend.py
+++ backend.py
@@ -1,8 +1,11 @@
 class Person():
     def __init__(self, name, dob, dod, des) -> None:
-        self.name = name
+        self.fullName = name
         self.dob = dob
         self.dod = dod
         self.description = des
-
+    
+    def getDescription():
+        pass
+    
 
