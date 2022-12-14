55cb1a5275d6c0c59a89bf1fdf6811ae6b8c7c13
Chris <97630660+therealstezzy@users.noreply.github.com>
2022-12-13 20:13:26 -0800

    organize

--- backend.py
+++ backend.py
@@ -1,18 +1,11 @@
-import pandas as pd
-
 class Person():
     def __init__(self, name, dob, dod, des) -> None:
         self.fullName = name
         self.dob = dob
         self.dod = dod
         self.description = des
-    
-    def getDescription(self):
-        return self.description
-        
 
-    
-data = [10,20,30,40,50,60]
-df = pd.DataFrame(data, columns=['Numbers'])
 
-print(df)
+    def __init__(self) -> None:
+        pass
+    
--- database.sql
+++
@@ -1,3 +0,0 @@
-SELECT NOW();
-
-/*CREATE DATABASE tombInfo;*/
